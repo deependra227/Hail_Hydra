108ce68771bb55a92812f176f470803e39f16e81
deependra227 <cdeep@iitk.ac.in>
2019-06-25 15:50:14 +0530

    upload

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_uikit/myapp.dart';
+
+void main() {
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
---
+++ lib/model/menu.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Menu {
+  String title;
+  IconData icon;
+  String image;
+  List<String> items;
+  BuildContext context;
+  Color menuColor;
+  String address;
+
+  Menu(
+      {this.title,
+      this.icon,
+      this.image,
+      this.items,
+      this.context,
+      this.menuColor = Colors.black,
+      this.address});
+}
