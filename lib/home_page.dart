bcb1d6275bfef9555b20166f64291b66b9223e52
vsaurabh04 <vsaurabh@iirk.ac.in>
2019-06-28 11:14:13 +0530

    added Sidebar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -58,7 +58,7 @@ List<Menu> menu;
     children: <Widget>[
 
       Text(
-        "Sponors",
+        "Doctors",
         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       )
     ],
@@ -78,7 +78,7 @@ List<Menu> menu;
       ),
       title: Row(
         children: <Widget>[
-          Text('SPONSORS')
+          Text('DOCTORS')
         ],
       ),
     ),
@@ -109,6 +109,45 @@ List<Menu> menu;
 
         ],
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('Options to be here'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.brown,
+                child: new Text("P"),
+              ),
+              
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              leading: Icon(Icons.home),
+              onTap: () {
+              // Update the state of the app.
+              // ...
+              Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Page'),
+              leading: Icon(Icons.navigate_next),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+
+              },
+            ),
+          ],
+        ),
+        elevation: 16.0,
+        ),
     );
   }
 }
